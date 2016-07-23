962ef2cc20dd7c63a4316c75a3b75c673de39f3a
Gregory Hedrick <gregory@pixiitech.net>
2016-07-23 16:21:19 -0400

    Finished tide chart scraper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -8,7 +8,9 @@ module WelcomeHelper
       (options[:day]) ? url += "&d=#{options[:day]}" : nil
       response = HTTParty.get(url)
       parsed_html = Nokogiri::HTML.parse(response.body)
-      @predictions = parsed_html.css(".predictions-table").first.children.first.to_s.split("\n")
+      tides = parsed_html.css(".predictions-table").first.children.first.to_s.split("\n").select do |x|
+        (x[0, 4] == ("%04d" % options[:year])) && (x[5, 2] == ("%02d" % options[:month])) && (x[8, 2] == ("%02d" % options[:day]))
+      end
     end
   end
 end
