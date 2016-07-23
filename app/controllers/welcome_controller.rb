eb683d7f1c092ee7d6623d56d60f71fdd37335a4
Gregory Hedrick <gregory@pixiitech.net>
2016-07-23 17:38:20 -0400

    Added final calculations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,10 @@ class WelcomeController < ApplicationController
   end
 
   def form_submit
+    start_location = Location.find_by(name: params[:start_location])
+    end_location = Location.find_by(name: params[:end_location])
+    @distance = start_location.calculate_distance_to(end_location)
+    @tide_info = WelcomeHelper::TidesAPI.new(@start_location, year: params[:start_time][6,4], month: params[:start_time][0,2], day: params[:start_time][3,2]).tides.join("<br>")
     @destination = "The Atlantic Ocean"
     @calculated_time = "12pm"
 
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,16 +1,20 @@
 module WelcomeHelper
   class TidesAPI
-    attr_reader :predictions
+    attr_reader :tides
+
     def initialize(loc_id, options)
-      url = "http://tides.mobilegeographics.com/locations/#{loc_id}.html"
+      url = "http://tides.mobilegeographics.com/locations/152.html"
+      p "Tides API URL: #{url}"
       (options[:year]) ? url += "?y=#{options[:year]}" : nil
       (options[:month]) ? url += "&m=#{options[:month]}" : nil
       (options[:day]) ? url += "&d=#{options[:day]}" : nil
       response = HTTParty.get(url)
       parsed_html = Nokogiri::HTML.parse(response.body)
-      tides = parsed_html.css(".predictions-table").first.children.first.to_s.split("\n").select do |x|
+      @tides = parsed_html.css(".predictions-table").first.children.first.to_s.split("\n").select do |x|
         (x[0, 4] == ("%04d" % options[:year])) && (x[5, 2] == ("%02d" % options[:month])) && (x[8, 2] == ("%02d" % options[:day]))
       end
+      p @tides
+      return @tides
     end
   end
 end
